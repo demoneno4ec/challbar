ce5d8373bb828dbccf9eaf14b02173ad52f9ce20
maxim.pestov <maxim.pestov@codezavod.ru>
2019-08-05 16:06:38 +0300

    change fb pixel

--- include/header.php
+++ include/header.php
@@ -38,11 +38,11 @@ $page = new PageSetting();
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '460285507873294');
+        fbq('init', '868536793516526');
         fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=460285507873294&ev=PageView&noscript=1"
+                   src="https://www.facebook.com/tr?id=868536793516526&ev=PageView&noscript=1"
         /></noscript>
     <!-- End Facebook Pixel Code -->
 </head>
